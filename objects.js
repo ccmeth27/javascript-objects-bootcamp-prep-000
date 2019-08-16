980d3cc0ee5e17d1901a740ec2b6252da13d92f2
Connor Meth <connormeth27@gmail.com>
2019-08-16 17:50:41 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,14 @@
-var meals = {
-  breakfast: 'avocado',
-  lunch: 'avocado',
-  dinner: 'avocado'
-};
-console.log(meals.breakfast)
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
+  obj[key] = value
+ 
+  return obj
+}
+ 
+const recipe = { eggs: 3 }
+ 
+destructivelyUpdateObjectWithKeyAndValue(recipe, 'flour', '3 cups')
+// returns { eggs: 3, flour: '3 cups' }
+ 
+// but also:
+ 
+recipe // { eggs: 3, flour: '3 cups' }
