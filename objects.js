283dadf0ad859f935113a54226e07f11ffcd5586
Connor Meth <connormeth27@gmail.com>
2019-08-16 17:57:31 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,14 +1,3 @@
-function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
-  obj[key] = value
- 
-  return obj
+function updateObjectWithObject(targetObject, updatesObject) {
+  return Object.assign({}, targetObject, updatesObject);
 }
- 
-const recipe = { eggs: 3 }
- 
-destructivelyUpdateObjectWithKeyAndValue(recipe, 'flour', '3 cups')
-// returns { eggs: 3, flour: '3 cups' }
- 
-// but also:
- 
-recipe // { eggs: 3, flour: '3 cups' }
